09bbc2e0911f79d3071f15adec94b3c51f12d173
admin <khmo492007@gmail.com>
2023-05-26 22:07:13 +0300

    fix error and get from db

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',{'Pro':Product.objects.all()})
